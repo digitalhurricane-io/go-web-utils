37817a10851125f05ecb4315aed0c9f4c996426b
Paul Hendricks <paul@digitalhurricane.io>
2022-12-12 14:15:44 -0700

    modify RandomString function

--- random_string.go
+++ random_string.go
@@ -4,17 +4,23 @@ import (
 	"math/rand"
 )
 
-var letters = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+var Letters = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
 
-func RandomString(n int) string {
+var LowercaseLetters = []rune("abcdefghijklmnopqrstuvwxyz")
+
+var UpperCaseLetters = []rune("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+
+var Numbers = []rune("0123456789")
+
+func RandomString(n int, characterSet []rune) string {
 	b := make([]rune, n)
 	for i := range b {
-		b[i] = letters[rand.Intn(len(letters))]
+		b[i] = characterSet[rand.Intn(len(characterSet))]
 	}
 	return string(b)
 }
 
 // RandIntRange inclusive
 func RandIntRange(min, max int) int {
-	return rand.Intn(max - min + 1) + min
+	return rand.Intn(max-min+1) + min
 }
